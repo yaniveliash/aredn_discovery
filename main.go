b2cd492bbec716ea75493be3751a2d6d49d1f3b0
Yaniv Eliash <yaniv.eliash@elastic.co>
2019-09-22 10:22:59 +0300

    html creation

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Nodes struct {
@@ -26,12 +28,17 @@ type Service struct {
 	Link     string `json:"link"`
 }
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func getUrl(url string) []byte {
 
 	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
+
 	defer resp.Body.Close()
 
 	buf := new(bytes.Buffer)
@@ -42,47 +49,79 @@ func getUrl(url string) []byte {
 }
 
 // getServices list all the visible services in the network
-func getService() {
+func getService() string {
 	var services Services
 
 	respByte := getUrl("http://localnode:8080/cgi-bin/sysinfo.json?services=1")
 
 	err := json.Unmarshal(respByte, &services)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Number of services in the network: ", len(services.Services))
-
+	check(err)
+	result := "<h1>AREDN IL SERVICES</h1>"
+	result = result + "<table><tr>"
 	for i := 0; i < len(services.Services); i++ {
-		fmt.Println(services.Services[i].Name)
-		fmt.Println(services.Services[i].Protocol)
-		fmt.Println(services.Services[i].Link)
-		fmt.Println("-------------")
+		result = result + "<th><a href=\""
+		result = result + (services.Services[i].Link)
+		result = result + "/\">"
+		result = result + (services.Services[i].Name)
+		result = result + "</a></th>"
 	}
+
+	result = result + "</tr></table><br>"
+	result = result + ("Number of services in the network: ")
+	result = result + strconv.Itoa(len(services.Services))
+
+	return result
 }
 
 // getNodes list all the visible nodes in the network
-func getNodes() {
+func getNodes() string {
 	var nodes Nodes
 
 	respByte := getUrl("http://localnode:8080/cgi-bin/sysinfo.json?hosts=1")
 	err := json.Unmarshal(respByte, &nodes)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
-	fmt.Println("Number of hosts in the network: ", len(nodes.Nodes))
-
+	//Creating table
+	result := "<h1>AREDN IL NODES</h1>"
+	result = result + "<table><tr>"
 	for i := 0; i < len(nodes.Nodes); i++ {
-		fmt.Println(nodes.Nodes[i].Name)
-		fmt.Println(nodes.Nodes[i].Ip)
-		fmt.Println("-------------")
+		result = result + "<th><a href=\"http://"
+		result = result + (nodes.Nodes[i].Name)
+		result = result + "/\">"
+		result = result + (nodes.Nodes[i].Name)
+		result = result + "</a></th>"
 	}
+
+	result = result + "</tr></table><br>"
+	result = result + "Number of hosts in the network: "
+	result = result + strconv.Itoa(len(nodes.Nodes))
+	result = result + "<br><br>"
+
+	return result
 }
 
 func main() {
+	if _, err := os.Stat("www"); os.IsNotExist(err) {
+		err := os.Mkdir("www", 0755)
+		check(err)
+	}
 
-	getNodes()
-	getService()
+	f, err := os.Create("www/nodes.html")
+	check(err)
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+
+	w.WriteString("<style>.content { max-width: 500px; margin: auto; } ")
+	w.WriteString("table { border-collapse: collapse; } ")
+	w.WriteString("table, th, td { border: 1px solid black; } ")
+	w.WriteString("th, td { padding: 15px; text-align: left; } </style>")
+	w.WriteString("<body><div class=\"content\">")
+
+	w.WriteString(getNodes())
+	w.WriteString(getService())
+
+	w.WriteString("</div></body>")
+
+	w.Flush()
 }
